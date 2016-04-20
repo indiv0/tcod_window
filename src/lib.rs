e55b3665604b2c8b939bd4663f4c3d65d7af4aec
Nikita Pekin <contact@nikitapek.in>
2016-04-20 05:57:49 -0400

    feat: update to latest Rust
    Update `pistoncore-input` to 0.0.5.
    Update `pistoncore-window` to 0.0.12.
    Remove `quack` dependency.
    Update `tcod` to 0.9.0.
    Re-organize `Cargo.toml`.
    
    Remove `quack` macro code.
    
    Implement `BuildFromWindowSettings` for `TcodWindow`.
    Implement `Window` for `TcodWindow`.
    Implement `AdvancedWindow` for `TcodWindow`.
    
    Add `clippy` as an optional dependency.
    Add `nightly-testing` feature to enable `clippy` dependency.
    Add `clippy` plugin to `lib.rs` to run `clippy` when building with the
    `nightly-testing` feature.
    Add cfg_attr to `tcod_map_mouse` function to allow the `if_not_else` `clippy`
    lint when building with the `clippy` feature enabled.
    
    Add `rustfmt.toml` with reasonable default configuration.
    
    Update `tcod_map_mouse` and `tcod_map_key` functions for latest `tcod`.
    Update `TcodWindow` struct, and the `TcodWindow` methods `new` and
    `check_for_event` for the latest `piston` and `tcod`.
    
    Add a `with_console` method for `TcodWindow`.
    Modify `TcodWindow` to store the `Root` in a `Rc<RefCell<Root>>`.
    Simplify the `TcodWindow` `mouse_state_prev` initialization code.
    
    Clean up `use` statements in library root and in functions and methods.
    Fix clippy lints pertaining to unnecessary `return` statements.
    Replace `match` statements with `if let` blocks where possible.
    
    Improve `tcod_map_key` by implementing support for printable characters and by
    simplifying the `match` statement logic.
    
    Apply `rustfmt` to the project.
    
    Remove unnecessary comments.
    
    Closes #6.

--- src/lib.rs
+++ src/lib.rs
@@ -1,242 +1,234 @@
-#![feature(core)]
+#![cfg_attr(feature = "nightly-testing", feature(plugin))]
+#![cfg_attr(feature = "clippy", plugin(clippy))]
 
 //! A TCOD back-end for the Piston game engine.
 
 extern crate input;
 #[macro_use]
-extern crate quack;
 extern crate tcod;
 extern crate window;
 
-use input::{ keyboard, Button, MouseButton, Input, Motion };
-use quack::Associative;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use input::{Input, MouseButton};
+use input::keyboard::Key as PistonKey;
+use tcod::input::{Key as TcodKey, KeyCode, Mouse};
 use tcod::Console;
-use window::{
-    WindowSettings,
-    ShouldClose, Size, PollEvent, SwapBuffers,
-    CaptureCursor, DrawSize, Title, ExitOnEsc
-};
+use tcod::console::Root;
+use window::{AdvancedWindow, BuildFromWindowSettings, Size, Window, WindowSettings};
 
 pub struct TcodWindow {
-    pub window: tcod::Console,
+    pub window: Rc<RefCell<Root>>,
     title: String,
     should_close: bool,
     mouse_relative: Option<(f64, f64)>,
-    mouse_state_prev: tcod::MouseState,
+    mouse_state_prev: Mouse,
     exit_on_esc: bool,
 }
 
 impl TcodWindow {
-    pub fn new(settings: WindowSettings) -> TcodWindow {
-        let window = Console::init_root(
-            settings.size[0] as i32,
-            settings.size[1] as i32,
-            &*settings.title,
-            false
-        );
+    pub fn new(settings: WindowSettings) -> Self {
+        let console = Root::initializer()
+                          .size(settings.get_size().width as i32,
+                                settings.get_size().height as i32)
+                          .title(settings.get_title())
+                          .init();
+        let console = Rc::new(RefCell::new(console));
 
+        Self::with_console(console, settings)
+    }
+
+    pub fn with_console(console: Rc<RefCell<Root>>, settings: WindowSettings) -> Self {
         TcodWindow {
-            window: window,
-            title: settings.title,
+            window: console,
+            title: settings.get_title(),
             should_close: false,
             mouse_relative: None,
-            // TODO: determine if there's a better way to initialize the
-            // MouseState.
-            mouse_state_prev: tcod::MouseState {
-                x: 0isize,
-                y: 0isize,
-                dx: 0isize,
-                dy: 0isize,
-                cx: 0isize,
-                cy: 0isize,
-                dcx: 0isize,
-                dcy: 0isize,
-                lbutton: false,
-                rbutton: false,
-                mbutton: false,
-                lbutton_pressed: false,
-                rbutton_pressed: false,
-                mbutton_pressed: false,
-                wheel_up: false,
-                wheel_down: false,
-            },
-            exit_on_esc: settings.exit_on_esc,
+            mouse_state_prev: Mouse::default(),
+            exit_on_esc: settings.get_exit_on_esc(),
         }
     }
 
     fn poll_event(&mut self) -> Option<Input> {
-        use tcod::KeyCode;
-        use tcod::Key::Special;
-        use tcod::system::Event;
+        use input::{Button, Motion};
+        use input::Input::{Move, Press, Release};
+        use tcod::input::{ANY, KEY_PRESS, KEY_RELEASE, MOUSE_MOVE, MOUSE_PRESS, MOUSE_RELEASE,
+                          check_for_event};
+        use tcod::input::Event::{Key, Mouse};
 
-        match self.mouse_relative {
-            Some((x, y)) => {
-                self.mouse_relative = None;
-                return Some(Input::Move(Motion::MouseRelative(x, y)));
-            }
-            None => {}
+        if let Some((x, y)) = self.mouse_relative {
+            self.mouse_relative = None;
+            return Some(Move(Motion::MouseRelative(x, y)));
         }
-        let (flags, event) = tcod::system::check_for_event(tcod::ANY);
-        match (flags, event) {
-            (tcod::KEY_PRESS, Event::Key(ref key_state)) => {
-                if self.exit_on_esc
-                && key_state.key == Special(KeyCode::Escape) {
+
+        match check_for_event(ANY) {
+            Some((KEY_PRESS, Key(ref key_state))) => {
+                if self.exit_on_esc && key_state.code == KeyCode::Escape {
                     self.should_close = true;
+                    None
                 } else {
-                    return Some(Input::Press(Button::Keyboard(tcod_map_key(key_state.key))));
+                    Some(Press(Button::Keyboard(tcod_map_key(*key_state))))
                 }
             },
-            (tcod::KEY_RELEASE, Event::Key(ref key_state)) => {
-                return Some(Input::Release(Button::Keyboard(tcod_map_key(key_state.key))));
+            Some((KEY_RELEASE, Key(ref key_state))) => {
+                Some(Release(Button::Keyboard(tcod_map_key(*key_state))))
             },
-            (tcod::MOUSE_PRESS, Event::Mouse(ref mouse_state)) => {
+            Some((MOUSE_PRESS, Mouse(ref mouse_state))) => {
                 let button = tcod_map_mouse(self.mouse_state_prev, mouse_state);
-                self.mouse_state_prev = *mouse_state.clone();
-                return Some(Input::Press(Button::Mouse(button)));
+                self.mouse_state_prev = *mouse_state;
+                Some(Press(Button::Mouse(button)))
             },
-            (tcod::MOUSE_RELEASE, Event::Mouse(ref mouse_state)) => {
+            Some((MOUSE_RELEASE, Mouse(ref mouse_state))) => {
                 let button = tcod_map_mouse(self.mouse_state_prev, mouse_state);
-                self.mouse_state_prev = *mouse_state.clone();
-                return Some(Input::Release(Button::Mouse(button)));
+                self.mouse_state_prev = *mouse_state;
+                Some(Release(Button::Mouse(button)))
             },
-            (tcod::MOUSE_MOVE, Event::Mouse(ref mouse_state)) => {
-                // TODO: figure out why mouse_state.dx and mouse_state.dy
-                // don't work.
-                self.mouse_relative = Some((
-                    (mouse_state.x - self.mouse_state_prev.x) as f64,
-                    (mouse_state.y - self.mouse_state_prev.y) as f64
-                ));
-                self.mouse_state_prev = *mouse_state.clone();
-                return Some(Input::Move(Motion::MouseCursor(mouse_state.x as f64, mouse_state.y as f64)));
+            Some((MOUSE_MOVE, Mouse(ref mouse_state))) => {
+                self.mouse_relative = Some(((mouse_state.x - self.mouse_state_prev.x) as f64,
+                                            (mouse_state.y - self.mouse_state_prev.y) as f64));
+                self.mouse_state_prev = *mouse_state;
+                Some(Move(Motion::MouseCursor(mouse_state.x as f64, mouse_state.y as f64)))
             },
-            (_, _) => {}
-        }
-        None
-    }
-}
-
-quack! {
-    _obj: TcodWindow[]
-    get:
-        fn () -> ShouldClose [] { ShouldClose(_obj.should_close) }
-        fn () -> Size [] {
-            let (w, h) = (_obj.window.width(), _obj.window.height());
-            Size([w as u32, h as u32])
-        }
-        fn () -> DrawSize [] {
-            //let (w, h) = _obj.window.get_drawable_size();
-            let (w, h) = (_obj.window.width(), _obj.window.height());
-            DrawSize([w as u32, h as u32])
-        }
-        // TODO: Remove the .clone()
-        fn () -> Title [] { Title(_obj.title.clone()) }
-        fn () -> ExitOnEsc [] { ExitOnEsc(_obj.exit_on_esc) }
-    set:
-        fn (val: CaptureCursor) [] {
-            //sdl2::mouse::set_relative_mouse_mode(val.0)
-        }
-        fn (val: ShouldClose) [] { _obj.should_close = val.0 }
-        fn (val: Title) [] {
-            Console::set_window_title(&*val.0);
-            _obj.title = val.0
-        }
-        fn (val: ExitOnEsc) [] { _obj.exit_on_esc = val.0 }
-    action:
-        fn (__: SwapBuffers) -> () [] {
-            //_obj.window.gl_swap_window()
-            Console::flush()
-        }
-        fn (__: PollEvent) -> Option<Input> [] { _obj.poll_event() }
-}
-
-impl Associative for (PollEvent, TcodWindow) {
-    type Type = Input;
-}
-
-pub fn tcod_map_key(key: tcod::Key) -> keyboard::Key {
-    use input::keyboard::Key;
-    use std::num::FromPrimitive;
-    match key {
-        tcod::Key::Printable(keycode) => FromPrimitive::from_u64(keycode as u64).unwrap(),
-        tcod::Key::Special(keycode) => {
-            match keycode {
-                tcod::KeyCode::NoKey => Key::Unknown,
-                tcod::KeyCode::Escape => Key::Escape,
-                tcod::KeyCode::Backspace => Key::Backspace,
-                tcod::KeyCode::Tab => Key::Tab,
-                tcod::KeyCode::Enter => Key::Return,
-                tcod::KeyCode::Shift => Key::LShift, // TODO: find RShift
-                tcod::KeyCode::Control => Key::LCtrl,
-                tcod::KeyCode::Alt => Key::LAlt,
-                tcod::KeyCode::Pause => Key::Pause,
-                tcod::KeyCode::Capslock => Key::CapsLock,
-                tcod::KeyCode::Pageup => Key::PageUp,
-                tcod::KeyCode::Pagedown => Key::PageDown,
-                tcod::KeyCode::End => Key::End,
-                tcod::KeyCode::Home => Key::Home,
-                tcod::KeyCode::Up => Key::Up,
-                tcod::KeyCode::Left => Key::Left,
-                tcod::KeyCode::Right => Key::Right,
-                tcod::KeyCode::Down => Key::Down,
-                tcod::KeyCode::PrintScreen => Key::PrintScreen,
-                tcod::KeyCode::Insert => Key::Insert,
-                tcod::KeyCode::Delete => Key::Delete,
-                tcod::KeyCode::LeftWin => Key::LGui,
-                tcod::KeyCode::RightWin => Key::RGui,
-                tcod::KeyCode::Apps => Key::Application,
-                // The numbers on the alphanum section of the keyboard
-                tcod::KeyCode::Number0 => Key::D0,
-                tcod::KeyCode::Number1 => Key::D1,
-                tcod::KeyCode::Number2 => Key::D2,
-                tcod::KeyCode::Number3 => Key::D3,
-                tcod::KeyCode::Number4 => Key::D4,
-                tcod::KeyCode::Number5 => Key::D5,
-                tcod::KeyCode::Number6 => Key::D6,
-                tcod::KeyCode::Number7 => Key::D7,
-                tcod::KeyCode::Number8 => Key::D8,
-                tcod::KeyCode::Number9 => Key::D9,
-                // The numbers on the numeric keypad
-                tcod::KeyCode::NumPad0 => Key::NumPad0,
-                tcod::KeyCode::NumPad1 => Key::NumPad1,
-                tcod::KeyCode::NumPad2 => Key::NumPad2,
-                tcod::KeyCode::NumPad3 => Key::NumPad3,
-                tcod::KeyCode::NumPad4 => Key::NumPad4,
-                tcod::KeyCode::NumPad5 => Key::NumPad5,
-                tcod::KeyCode::NumPad6 => Key::NumPad6,
-                tcod::KeyCode::NumPad7 => Key::NumPad7,
-                tcod::KeyCode::NumPad8 => Key::NumPad8,
-                tcod::KeyCode::NumPad9 => Key::NumPad9,
-                tcod::KeyCode::NumPadAdd => Key::NumPadPlus,
-                tcod::KeyCode::NumPadSubtract => Key::NumPadMinus,
-                tcod::KeyCode::NumPadDivide => Key::NumPadDivide,
-                tcod::KeyCode::NumPadMultiply => Key::NumPadMultiply,
-                tcod::KeyCode::NumPadDecimal => Key::NumPadDecimal,
-                tcod::KeyCode::NumPadEnter => Key::NumPadEnter,
-                tcod::KeyCode::F1 => Key::F1,
-                tcod::KeyCode::F2 => Key::F2,
-                tcod::KeyCode::F3 => Key::F3,
-                tcod::KeyCode::F4 => Key::F4,
-                tcod::KeyCode::F5 => Key::F5,
-                tcod::KeyCode::F6 => Key::F6,
-                tcod::KeyCode::F7 => Key::F7,
-                tcod::KeyCode::F8 => Key::F8,
-                tcod::KeyCode::F9 => Key::F9,
-                tcod::KeyCode::F10 => Key::F10,
-                tcod::KeyCode::F11 => Key::F11,
-                tcod::KeyCode::F12 => Key::F12,
-                tcod::KeyCode::NUMLOCK => Key::NumLockClear,
-                tcod::KeyCode::SCROLLLOCK => Key::ScrollLock,
-                tcod::KeyCode::Spacebar => Key::Space,
-                // TODO: check if this is correct.
-                tcod::KeyCode::Char => Key::Unknown,
-            }
+            _ => None,
         }
     }
 }
 
-pub fn tcod_map_mouse(prev_state: tcod::MouseState, state: &tcod::MouseState) -> MouseButton {
-    if prev_state.lbutton != state.lbutton { return MouseButton::Left }
-    else if prev_state.rbutton != state.rbutton { return MouseButton::Right }
-    else if prev_state.mbutton != state.mbutton { return MouseButton::Middle }
-    else { return MouseButton::Unknown }
+impl BuildFromWindowSettings for TcodWindow {
+    fn build_from_window_settings(settings: WindowSettings) -> Result<Self, String> {
+        Ok(TcodWindow::new(settings))
+    }
+}
+
+impl Window for TcodWindow {
+    type Event = Input;
+
+    fn should_close(&self) -> bool {
+        self.should_close
+    }
+    fn set_should_close(&mut self, value: bool) {
+        self.should_close = value;
+    }
+    fn swap_buffers(&mut self) {
+        self.window.borrow_mut().flush();
+    }
+    fn size(&self) -> Size {
+        let window = self.window.borrow();
+        Size {
+            width: window.width() as u32,
+            height: window.height() as u32,
+        }
+    }
+    fn poll_event(&mut self) -> Option<Input> {
+        self.poll_event()
+    }
+    fn draw_size(&self) -> Size {
+        let window = self.window.borrow();
+        Size {
+            width: window.width() as u32,
+            height: window.height() as u32,
+        }
+    }
+}
+
+impl AdvancedWindow for TcodWindow {
+    fn get_title(&self) -> String {
+        self.title.clone()
+    }
+    fn set_title(&mut self, value: String) {
+        self.window.borrow_mut().set_window_title(&value);
+        self.title = value
+    }
+    fn get_exit_on_esc(&self) -> bool {
+        self.exit_on_esc
+    }
+    fn set_exit_on_esc(&mut self, value: bool) {
+        self.exit_on_esc = value;
+    }
+    fn set_capture_cursor(&mut self, _value: bool) {}
+}
+
+pub fn tcod_map_key(key: TcodKey) -> PistonKey {
+    match key.code {
+        KeyCode::NoKey => PistonKey::Unknown,
+        KeyCode::Escape => PistonKey::Escape,
+        KeyCode::Backspace => PistonKey::Backspace,
+        KeyCode::Tab => PistonKey::Tab,
+        KeyCode::Enter => PistonKey::Return,
+        KeyCode::Shift => PistonKey::LShift,
+        KeyCode::Control => PistonKey::LCtrl,
+        KeyCode::Alt => PistonKey::LAlt,
+        KeyCode::Pause => PistonKey::Pause,
+        KeyCode::CapsLock => PistonKey::CapsLock,
+        KeyCode::PageUp => PistonKey::PageUp,
+        KeyCode::PageDown => PistonKey::PageDown,
+        KeyCode::End => PistonKey::End,
+        KeyCode::Home => PistonKey::Home,
+        KeyCode::Up => PistonKey::Up,
+        KeyCode::Left => PistonKey::Left,
+        KeyCode::Right => PistonKey::Right,
+        KeyCode::Down => PistonKey::Down,
+        KeyCode::PrintScreen => PistonKey::PrintScreen,
+        KeyCode::Insert => PistonKey::Insert,
+        KeyCode::Delete => PistonKey::Delete,
+        KeyCode::LeftWin => PistonKey::LGui,
+        KeyCode::RightWin => PistonKey::RGui,
+        KeyCode::Apps => PistonKey::Application,
+        // The numbers on the numeric keypad
+        KeyCode::NumPad0 => PistonKey::NumPad0,
+        KeyCode::NumPad1 => PistonKey::NumPad1,
+        KeyCode::NumPad2 => PistonKey::NumPad2,
+        KeyCode::NumPad3 => PistonKey::NumPad3,
+        KeyCode::NumPad4 => PistonKey::NumPad4,
+        KeyCode::NumPad5 => PistonKey::NumPad5,
+        KeyCode::NumPad6 => PistonKey::NumPad6,
+        KeyCode::NumPad7 => PistonKey::NumPad7,
+        KeyCode::NumPad8 => PistonKey::NumPad8,
+        KeyCode::NumPad9 => PistonKey::NumPad9,
+        KeyCode::NumPadAdd => PistonKey::NumPadPlus,
+        KeyCode::NumPadSubtract => PistonKey::NumPadMinus,
+        KeyCode::NumPadDivide => PistonKey::NumPadDivide,
+        KeyCode::NumPadMultiply => PistonKey::NumPadMultiply,
+        KeyCode::NumPadDecimal => PistonKey::NumPadDecimal,
+        KeyCode::NumPadEnter => PistonKey::NumPadEnter,
+        KeyCode::F1 => PistonKey::F1,
+        KeyCode::F2 => PistonKey::F2,
+        KeyCode::F3 => PistonKey::F3,
+        KeyCode::F4 => PistonKey::F4,
+        KeyCode::F5 => PistonKey::F5,
+        KeyCode::F6 => PistonKey::F6,
+        KeyCode::F7 => PistonKey::F7,
+        KeyCode::F8 => PistonKey::F8,
+        KeyCode::F9 => PistonKey::F9,
+        KeyCode::F10 => PistonKey::F10,
+        KeyCode::F11 => PistonKey::F11,
+        KeyCode::F12 => PistonKey::F12,
+        KeyCode::NumLock => PistonKey::NumLockClear,
+        KeyCode::ScrollLock => PistonKey::ScrollLock,
+        KeyCode::Spacebar => PistonKey::Space,
+        // Characters
+        _ => {
+            let key_as_u32 = match key.printable {
+                // We need to convert the uppercase characters to be lowercase.
+                key @ 'A' ... 'Z' => key.to_lowercase().next().unwrap() as u32,
+                key => key as u32,
+            };
+
+            key_as_u32.into()
+        },
+    }
+}
+
+#[cfg_attr(feature = "clippy", allow(if_not_else))]
+pub fn tcod_map_mouse(prev_state: Mouse, state: &Mouse) -> MouseButton {
+    if prev_state.lbutton != state.lbutton {
+        MouseButton::Left
+    } else if prev_state.rbutton != state.rbutton {
+        MouseButton::Right
+    } else if prev_state.mbutton != state.mbutton {
+        MouseButton::Middle
+    } else {
+        MouseButton::Unknown
+    }
 }
